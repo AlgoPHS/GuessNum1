3dacb92f003f5982705947c63e6477a373babdea
Walorus <pvass153@gmail.com>
2016-01-11 12:29:52 -0500

    creating the main class file with all methods we will need

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class main{
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		int guess = Integer.parseInt(x.nextLine());
+		String response = checkInt(guess);
+		int answer = genNumber();
+	}
+	public String checkInt(int guess){
+
+	}
+	public int genNumber(){
+		
+	}
+}
