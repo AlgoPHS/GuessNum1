029052db278cebd6b87e6c3414ad0b3ab95a4272
Walorus <pvass153@gmail.com>
2016-01-11 12:42:20 -0500

    made changes to gen and main method

--- main.java
+++ main.java
@@ -2,17 +2,20 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class main{
-	private
 	public static void main(String[] args) {
 		Scanner x = new Scanner(System.in);
 		int guess = Integer.parseInt(x.nextLine());
 		int answer = genNumber();
 		String response = checkInt(guess,answer);
+		while(!response.equals("winner winner chicken dinner")){
+			
+		}
 	}
 	public String checkInt(int guess){
 
 	}
 	public int genNumber(){
-		Random
+		Random gen = new Random();
+		return gen.nextInt(100);
 	}
 }
