8042272df6dee7ad73cde81c6feff81a61915470
Walorus <pvass153@gmail.com>
2016-01-11 12:37:22 -0500

    made changes

--- main.java
+++ main.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+import java.util.Random;
+
 public class main{
+	private
 	public static void main(String[] args) {
 		Scanner x = new Scanner(System.in);
 		int guess = Integer.parseInt(x.nextLine());
-		String response = checkInt(guess);
 		int answer = genNumber();
+		String response = checkInt(guess,answer);
 	}
 	public String checkInt(int guess){
 
 	}
 	public int genNumber(){
-		
+		Random
 	}
 }
