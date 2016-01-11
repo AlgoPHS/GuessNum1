6fd137cea0c8183643ae9918c01ec118c0f58ffc
Walorus <pvass153@gmail.com>
2016-01-11 12:50:52 -0500

    made final changes to main method

--- main.java
+++ main.java
@@ -3,17 +3,28 @@ import java.util.Random;
 
 public class main{
 	public static void main(String[] args) {
-		Scanner x = new Scanner(System.in);
-		int guess = Integer.parseInt(x.nextLine());
-		int answer = genNumber();
-		String response = checkInt(guess,answer);
-		while(!response.equals("winner winner chicken dinner")){
-			
+		start=true;
+		while(start=true){
+			Scanner x = new Scanner(System.in);
+			int count=0;
+			int guess = Integer.parseInt(x.nextLine());
+			int answer = genNumber();
+			String response = checkInt(guess,answer);
+			while(!response.equals("winner winner chicken dinner")){
+				count++;
+				System.out.println(response);
+				guess = Integer.parseInt(x.nextLine());
+				response = checkInt(guess,answer);
+			}
+			System.out.println("You got it right! It only took you "+count+" attempts!");
+			System.out.println("Do you want to continue? (y/n)");
+			if(x.nextLine().equals("n").ignoreCase())
+				start=false;
 		}
 	}
 	public String checkInt(int guess, int answer){
 	String response1;
-	if(guess=answer)
+	if(guess==answer)
 		response1 = "winner winner chicken dinner";
 	if(guess>answer)
 		response1 = "too high";
